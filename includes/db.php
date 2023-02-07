f97ee0e09ef22ad0985d8232ef99f7f7f7447880
jlrguy2017 <80444619+jlrguy2017@users.noreply.github.com>
2023-02-06 21:11:28 -0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,5 @@
-<?php 
-
-$conn = mysqli_connect('localhost', 'root', '', 'music');
-
-if(!$conn){
+<?php
+$conn = pg_connect("host=localhost port=5432 dbname=music user=music");
+if (!$conn) {
     echo "Database connection failed";
 }
